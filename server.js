0fb6e2f18809d29b5d4319d7750bd981edbaa776
Abuye Mamuye <abuyeye3@gmail.com>
2021-02-16 21:17:34 -0800

    add file on heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+ app.use(express.static("assets"));
+
+ require("./routes/routes")(app);
+
+ app.listen(PORT, function () {
+     console.log("App now Listening On: http://localhost:" + PORT);
+ });
